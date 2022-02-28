38331c7744d15bf321a004a83cfb3205208cddb1
Jayrajsinh Solanki <jayrajsinhsolanki8008@gmail.com>
2022-02-28 11:57:12 +0530

    Hack was initially removed from jayrajsinh solanki

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-  <?php
-  ?>
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>PHP & Ajax CRUD Application</title>
+  <title>
+    <?php echo "PHP & Ajax CRUD Application"; ?>
+  </title>
   <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
